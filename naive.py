3b3e6329303d4cee53b0df21b2a401864ad7bbc9
Vy Bui <01bui@cardinalmail.cua.edu>
2016-05-04 21:47:18 -0400

    quiz

--- naive.py
+++ naive.py
@@ -6,10 +6,13 @@ def naive(a, b):
     while x > 0:
         z = z + y
         x = x - 1
+        if x == 20 and y == 12:
+            print z
     return z
 
-print naive(2,3)
-print naive(4,3)
+naive(63,12)
+#print naive(2,3)
+#print naive(4,3)
 
 # counting steps in naive as a function of a
 def time(a):
@@ -18,5 +21,5 @@ def time(a):
     steps = 3 + a*2
     return steps
 
-print time(2)
-print time(4)
+#print time(2)
+#print time(4)
