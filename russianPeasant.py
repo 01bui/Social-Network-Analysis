a7339719b90729f3e8fedf76bbe2f500bd2a62a8
Vy Bui <01bui@cardinalmail.cua.edu>
2016-05-11 21:23:54 -0400

    add details to answer quiz

--- russianPeasant.py
+++ russianPeasant.py
@@ -1,9 +1,12 @@
 def russian(a,b):
+    """a * b = x * y + z"""
     x = a
     y = b
     z = 0
     count = 0
     while x > 0:
+        if x == 7 and z == 84:
+            print y
         if x % 2 == 1:
             z = z + y
             count += 1
@@ -12,4 +15,5 @@ def russian(a,b):
         print x,y,z
     return z, count
 
-print russian(20,7)
+#print russian(20,7)
+print russian(63,12)
