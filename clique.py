c39c71216d0c9b171a0f331cb43ca0300caf76b2
Vy Bui <01bui@cardinalmail.cua.edu>
2016-05-12 21:18:35 -0400

    add time steps of clique

--- clique.py
+++ clique.py
@@ -6,10 +6,12 @@
 def count(n):
     # Your code here to count the units of time
     # it takes to execute clique
-    return 0
+    steps = 2 + n + n*(n-1)/2
+    return steps
 
 def clique(n):
     print "in a clique..."
     for j in range(n):
         for i in range(j):
             print i, "is friends with", j
+print count(4)
